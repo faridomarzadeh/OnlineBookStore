50f1cf3e10939a65a506a7be4a9d511bc884254b
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2024-04-03 00:57:47 +0430

    Order Aggregate started

---
+++ Ketabkhoob/Shop/Shop.Domain/OrderAgg/Order.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Domain.OrderAgg.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.OrderAgg
+{
+    public class Order
+    {
+        public long UserId { get; private set; }
+        public OrderStatus Status { get; private set; }
+        public OrderDiscount? Discount { get; private set; }
+        public List<OrderItem> Items { get; private set; }
+
+        public int TotalPrice => Items.Sum(item => item.TotalPrice);
+        public int ItemCount => Items.Count;
+    }
+
+    public enum OrderStatus
+    {
+        Pending,
+        Finalized,
+        Shipping,
+        Rejected
+    }
+    
+    public class OrderItem
+    {
+        public long OrderId { get;internal set; }
+        public int TotalPrice { get; private set; }
+    }
+}
