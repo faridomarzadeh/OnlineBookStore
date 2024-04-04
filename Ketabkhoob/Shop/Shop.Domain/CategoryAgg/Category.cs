37e9ff75a0c32c01691f5e6cd9184ed1022ade20
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2024-04-04 22:52:35 +0430

    Category Aggregate started

---
+++ Ketabkhoob/Shop/Shop.Domain/CategoryAgg/Category.cs
@@ -0,0 +1,47 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Common.Domain.Utils;
+using Common.Domain.ValueObjects;
+using Shop.Domain.CategoryAgg.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.CategoryAgg
+{
+    public class Category:AggregateRoot
+    {
+        public Category(string title, string slug, SeoData seoData, ICategoryDomainService categoryDomainService)
+        {
+            slug = slug.ToSlug();
+            Validate(title, slug, categoryDomainService);
+            Title = title;
+            Slug = slug;
+            SeoData = seoData;
+        }
+
+        public string Title { get; private set; }
+        public string Slug { get; private set; }
+        public SeoData SeoData { get; private set; }
+        public long? ParentId { get; private set; }
+
+        public void Edit(string title, string slug, ICategoryDomainService categoryDomainService)
+        {
+            slug = slug.ToSlug();
+            Validate(title, slug, categoryDomainService);
+            Title = title;
+            Slug = slug;
+        }
+
+        public void Validate(string title, string slug, ICategoryDomainService categoryDomainService)
+        {
+            NullOrEmptyDomainDataException.CheckString(title,nameof(title));
+            NullOrEmptyDomainDataException.CheckString(slug,nameof(slug));
+
+            if (slug != Slug && categoryDomainService.SlugExists(slug))
+                throw new DuplicatedSlugException(slug);
+        }
+    }
+}
